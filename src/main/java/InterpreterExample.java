2d5c251876259463d311aced429f686d131ab1db
Tymur Foshch <antariuz@gmail.com>
2021-08-16 22:51:43 +0300

    done?

--- src/main/java/InterpreterExample.java
+++ src/main/java/InterpreterExample.java
@@ -1,8 +1,23 @@
 public class InterpreterExample {
+    private final InterpreterContext context;
+
+    public InterpreterExample(InterpreterContext context) {
+        this.context = context;
+    }
+
+    public String interpret(String str) {
+        Expression expression;
+        double k = Double.parseDouble(str.substring(0, str.indexOf(" ")));
+        if (str.contains("K")) expression = new CelsiusToKelvinExpression(k);
+        else if (str.contains("F")) expression = new CelsiusToFahrenheitExpression(k);
+        else return str;
+        return expression.interpret(context);
+    }
+
     public static void main(String[] args) {
-        String str = "0 to F";
-        TerminalExpression terminalExpression = new TerminalExpression(new InterpreterContext());
-        System.out.println(str + " is " + terminalExpression.interpret(str));
+        String str = "0 to K";
+        InterpreterExample te = new InterpreterExample(new InterpreterContext());
+        System.out.println(str + " is " + te.interpret(str));
 
     }
 }
